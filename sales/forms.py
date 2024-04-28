87d6a6fcf5b87c913647cd626e63e8bd698a5077
KosDmit <kosdmit@hotmail.com>
2024-04-28 09:23:45 +0300

    sync commit

--- sales/forms.py
+++ sales/forms.py
@@ -4,7 +4,16 @@ from sales.models import Customer
 
 
 class CustomerForm(forms.ModelForm):
+    # template_name_div = 'forms/div.html'
+
     class Meta:
         model = Customer
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            # field.widget.attrs['placeholder'] = field.label
         
